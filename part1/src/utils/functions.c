dfd01f29a4cb1c2cdf19b5daf1268a65e0e8be9c
DavidEdri7 <davidedri5394@gmail.com>
2019-12-01 19:56:47 +0200

    minor changes

--- part1/src/utils/functions.c
+++ part1/src/utils/functions.c
@@ -2,20 +2,28 @@
 
 void printtree (node *tree, int tab){
     int i; 
-    for (i = 0; i< tab; i++)
-        printf ("\t");
     char* token = tree->token;
-        printf ("(%s\n", token);
-    if (tree -> left)
-        printtree (tree-> left, tab + 1);  
-    if (tree -> right)
-        printtree (tree-> right, tab + 1); 
-    for (i = 0; i< tab; i++)
+    for (i = 0; i< tab; i++){
         printf ("\t");
+    }
+    printf ("(%s\n", token);
+
+    if (tree -> left){
+        printtree (tree-> left, tab + 1);
+    }
+          
+    if (tree -> right){
+        printtree (tree-> right, tab + 1); 
+    }
+
+    for (i = 0; i< tab; i++){
+        printf ("\t");
+    }
+
     printf(")\n");
 }
 
-node *mknode    (char *token, node *left, node *right){
+node *mknode (char *token, node *left, node *right){
     node *newnode = (node*)malloc (sizeof(node));
     char    *newstr = (char*)malloc (sizeof(token)+1);
     strcpy (newstr, token);
